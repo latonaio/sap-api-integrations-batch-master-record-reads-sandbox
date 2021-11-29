bb66e023352c1a7727e4837e48e54e59d38340a4
Yurino Oga <yurino.o@latona.io>
2021-11-29 10:46:03 +0900

    release

---
+++ SAP_API_Output_Formatter/type.go
@@ -0,0 +1,29 @@
+package sap_api_output_formatter
+
+type BatchMasterRecord struct {
+	 ConnectionKey string `json:"connection_key"`
+	 Result        bool   `json:"result"`
+	 RedisKey      string `json:"redis_key"`
+	 Filepath      string `json:"filepath"`
+	 APISchema     string `json:"api_schema"`
+	 MaterialCode  string `json:"material_code"`
+	 Plant         string `json:"plant"`
+	 Batch         string `json:"batch_code"`
+	 Deleted       bool   `json:"deleted"`
+}
+	
+type Batch struct {
+     Material                  string `json:"Material"`
+     BatchIdentifyingPlant     string `json:"BatchIdentifyingPlant"`
+     Batch                     string `json:"Batch"`
+     Supplier                  string `json:"Supplier"`
+     BatchBySupplier           string `json:"BatchBySupplier"`
+     CountryOfOrigin           string `json:"CountryOfOrigin"`
+     RegionOfOrigin            string `json:"RegionOfOrigin"`
+     MatlBatchAvailabilityDate string `json:"MatlBatchAvailabilityDate"`
+     ShelfLifeExpirationDate   string `json:"ShelfLifeExpirationDate"`
+     ManufactureDate           string `json:"ManufactureDate"`
+	 CreationDateTime          string `json:"CreationDateTime"`
+     LastChangeDateTime        string `json:"LastChangeDateTime"`
+	 BatchIsMarkedForDeletion  bool   `json:"BatchIsMarkedForDeletion"`
+}
